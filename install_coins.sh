a9fdfba95ef8af5f68a3e3db995a135b7ce198e7
A-McMullen <61842123+A-McMullen@users.noreply.github.com>
2021-04-19 22:22:57 -0400

    Update install_coins.sh

--- install_coins.sh
+++ install_coins.sh
@@ -9,3 +9,18 @@ docker push $REGISTRY/busybox
 curl $REGISTRY/v2/_catalog
 git clone https://github.com/jpetazzo/container.training
 cd ~/container.training/stacks
+echo $NODEPORT
+
+# At this point, modify the dockercoins.yml file and replace 5000 with the port value of $NODEPORT. You can run echo $NODEPORT to find out this value.
+
+export TAG=v0.1
+docker-compose -f dockercoins.yml build
+docker-compose -f dockercoins.yml push
+kubectl create deployment redis --image=redis
+for SERVICE in hasher rng webui worker; do kubectl create deployment $SERVICE --image=$REGISTRY/$SERVICE:$TAG; done
+kubectl expose deployment redis --port 6379
+kubectl expose deployment rng --port 80
+kubectl expose deployment hasher --port 80
+kubectl expose deploy/webui --type=NodePort --port=80
+kubectl get svc
+
